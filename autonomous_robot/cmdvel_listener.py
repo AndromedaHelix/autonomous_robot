05512ea205b8683937b0eff3e9fa3b2fcb2e7d0e
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-09-17 17:56:09 -0600

    Added motor control

--- autonomous_robot/cmdvel_listener.py
+++ autonomous_robot/cmdvel_listener.py
@@ -3,19 +3,40 @@
 import rclpy 
 from rclpy.node import Node
 
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Vector3
+
+import RPi.GPIO as GPIO
 
 class CmdVelListener(Node): 
     def __init__(self):
         super().__init__('cmdvel_listener')
         self.subscription = self.create_subscription(
-            Twist,
+            Vector3,
             'cmdvel',
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
-    def listener_callback(self, msg:Twist):
-        self.get_logger().info('I heard: "%s"' % msg._linear)
+
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+
+        self.PWM = 15
+        self.IN3 = 11
+        self.IN4 = 13
+
+        GPIO.setup(self.IN3, GPIO.OUT) # Connected to PWMA
+        GPIO.setup(self.IN4, GPIO.OUT) # Connected to AIN2
+        GPIO.setup(self.PWM, GPIO.OUT) # Connected to AIN1
+
+        self.p = GPIO.PWM(self.PWM, 100)
+        self.p.start(0)
+              
+
+    def listener_callback(self, msg:Vector3):
+        self.p.ChangeDutyCycle(msg.z)
+        GPIO.output(self.IN3, GPIO.HIGH) # Set AIN1
+        GPIO.output(self.IN4, GPIO.LOW) # Set AIN2
+        self.get_logger().info('I heard: "%s"' % msg.z)
 
 def main(args=None):
     rclpy.init(args=args)
