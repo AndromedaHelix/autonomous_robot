826be08abbf7d8be632734d8ff45f568b90be129
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-09-18 21:55:41 -0600

    Cleaned up pins

--- autonomous_robot/cmdvel_listener.py
+++ autonomous_robot/cmdvel_listener.py
@@ -20,22 +20,37 @@ class CmdVelListener(Node):
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
 
-        self.PWM = 15
-        self.IN3 = 11
-        self.IN4 = 13
+        self.PWM = 11
+        self.IN3 = 13
+        self.IN4 = 15
+
+        self.PWM2 = 12
+        self.IN1 = 16
+        self.IN2 = 18
 
         GPIO.setup(self.IN3, GPIO.OUT) # Connected to PWMA
         GPIO.setup(self.IN4, GPIO.OUT) # Connected to AIN2
         GPIO.setup(self.PWM, GPIO.OUT) # Connected to AIN1
 
-        self.p = GPIO.PWM(self.PWM, 100)
+        GPIO.setup(self.IN1, GPIO.OUT) # Connected to PWMA
+        GPIO.setup(self.IN2, GPIO.OUT) # Connected to AIN2
+        GPIO.setup(self.PWM2, GPIO.OUT) # Connected to AIN1
+
+        self.p : GPIO.PWM = GPIO.PWM(self.PWM, 100)
         self.p.start(0)
+
+        self.p2 : GPIO.PWM = GPIO.PWM(self.PWM2, 100)
+        self.p2.start(0)
               
 
     def listener_callback(self, msg:Vector3):
         self.p.ChangeDutyCycle(msg.z)
         GPIO.output(self.IN3, GPIO.HIGH) # Set AIN1
         GPIO.output(self.IN4, GPIO.LOW) # Set AIN2
+
+        self.p2.ChangeDutyCycle(msg.z)
+        GPIO.output(self.IN1, GPIO.LOW) # Set AIN1
+        GPIO.output(self.IN2, GPIO.HIGH) # Set AIN2
         self.get_logger().info('I heard: "%s"' % msg.z)
 
 def main(args=None):
