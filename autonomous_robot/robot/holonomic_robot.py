c4573faf246053671235607e2b39bce95b239740
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-01-09 18:29:07 +0000

    Implemented kinematics

--- autonomous_robot/robot/holonomic_robot.py
+++ autonomous_robot/robot/holonomic_robot.py
@@ -1,23 +1,31 @@
 # Written by Juan Pablo Gutiérrez
-
+from typing import List
 from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Twist
 
 from autonomous_robot.motor import Motor
 
+from autonomous_robot.robot.utils.config import *
+from autonomous_robot.robot.kinematics import Kinematics
+
+
 class HolonomicRobot:
     def __init__(self, motor1 : Motor, motor2 : Motor, motor3 : Motor, motor4 : Motor):
+    
+        self.Kinematics = Kinematics(RobotConfig(0.05, 0.26))
+        
         self.motor1 = motor1
         self.motor2 = motor2
         self.motor3 = motor3
         self.motor4 = motor4
+
     
-    def moveRobot (self, movement : Vector3):
+    def moveRobot (self, movement : Twist):
         print("Moving Motor")
         
-        xSpeed = movement.x
-        ySpeed = movement.y
+        wheel_velocities : List[float] = self.Kinematics.getWheelsAngularVelocities(RobotVelocity(movement.linear.x , movement.linear.y, movement.angular.z))
 
-        self.motor1.moveMotor(ySpeed)
-        self.motor2.moveMotor(xSpeed)
-        self.motor3.moveMotor(xSpeed)
-        self.motor4.moveMotor(ySpeed)
+        self.motor1.moveMotor(wheel_velocities[0])
+        self.motor2.moveMotor(wheel_velocities[1])
+        self.motor3.moveMotor(wheel_velocities[2])
+        self.motor4.moveMotor(wheel_velocities[3])
